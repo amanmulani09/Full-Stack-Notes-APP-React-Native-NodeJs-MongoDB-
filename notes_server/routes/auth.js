1680bd780adcd7e6c39b8ef99dd92870e49af463
aman mulani <mulaniaman0504@gmail.com>
2024-01-26 17:13:12 +0530

    created signin and signup auth API's : 🚀

--- notes_server/routes/auth.js
+++ notes_server/routes/auth.js
@@ -2,8 +2,26 @@ const router = require('express').Router();
 const userSchema = require('../models/User');
 
 
-router.get('/login',(req,res)=>{
+router.post('/login',async(req,res)=>{
     
+    try{
+
+        const user = await userSchema.findOne({email:req.body.email})
+
+        !user && res.status(404).json({"message":"user not found",status:false});
+
+        const validatePassword = req.body.password === user.password;
+
+        !validatePassword && res.status(404).json({"message":"password",status:false});
+
+        if(validatePassword && user){
+            res.status(200).json(user);
+        }
+
+    }catch(error){
+         res.status(500).json(error);
+    }
+
 })
 
 router.post('/register',async(req,res)=>{
